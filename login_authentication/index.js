112fcb459ce7d1722c47384b18cb46ecf3c10afb
saisitaram2000 <saisitaramvaddavalli@gmail.com>
2020-06-27 16:23:09 +0530

    created Database and config.js for databse connection,created index.html&login.html

---
+++ login_authentication/index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var connection = require('./config');
+var app = express();
+
+var authenticateController = require('./controllers/authenticate-controller');
+var registerController = require('./controllers/register-controller');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.get('/login.html',function(req,res){
+    res.sendFile(__dirname+"/login.html");
+})
+
+
+
+
+
+app.listen(3000);
