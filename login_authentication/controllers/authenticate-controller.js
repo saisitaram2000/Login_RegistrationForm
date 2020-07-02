44ee8919d674a6deedb93a184f422be915555e13
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-07-02 20:50:16 +0530

    added bcrypt module for securing passwords i.e it encrypts(hash) the passwords

--- login_authentication/controllers/authenticate-controller.js
+++ login_authentication/controllers/authenticate-controller.js
@@ -1,6 +1,4 @@
-const fs = require('fs'); //allows to work with filesystem on ur computer
-const http = require('http');
-const url = require('url');
+var bcrypt = require('bcrypt');
 var connection = require('./../config');
 const { json } = require('body-parser');
 module.exports.authenticate = function(req,res){
@@ -15,21 +13,21 @@ module.exports.authenticate = function(req,res){
             })
         }else{
             if(results.length>0){
-               
-                if(password==results[0].password){
-                    const markup=`
-                     <center><h1>Welcome back ${results[0].name}</h1></center>;
-                    `;
-                    res.send(markup);
-                }else{
-                    res.json({
-                        status:false,
-                        message:'Email and password does not match'
+               bcrypt.compare(password,results[0].password,function(err,doesMatch){
+                    if(doesMatch){
+                        const markup=`
+                        <center><h1>Welcome back ${results[0].name}</h1></center>;
+                       `;
+                       res.send(markup);
+                    }else{
+                        res.json({
+                            status:false,
+                            message:'Email and password does not match'
+                        });
+                    }
                     });
-                }
-            }else{
-               res.send("Email doesnt exist");
-            }
+
+               }
         }
     });
 }
--- login_authentication/controllers/register-controller.js
+++ login_authentication/controllers/register-controller.js
@@ -1,16 +1,20 @@
 var express = require("express");
+var bcrypt = require("bcrypt");
 var connection = require('./../config');
  
 module.exports.register = function(req,res){
     var curr_time=new Date();
-   
+    const saltRounds=10;
+   bcrypt.hash(req.body.password,saltRounds,function(err,hashedPassword){
+
     var users={
         "name":req.body.name,
         "email":req.body.email,
-        "password":req.body.password,
+        "password":hashedPassword,
         "created_at":curr_time,
         "updated_at":curr_time
     }
+
     connection.query('INSERT INTO users SET ?',users,function(error,results,fields){
         if(error){
             console.log(error);
@@ -25,4 +29,6 @@ module.exports.register = function(req,res){
            res.send(markup);
         }
     });
+   });
+   
 }
