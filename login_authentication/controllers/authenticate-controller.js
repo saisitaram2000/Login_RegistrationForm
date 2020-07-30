ff496fcf2816dd9281acb0151156c6a1a7ddb939
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-07-30 17:17:34 +0530

    added dice game as welcome page if user login is successfull

--- login_authentication/controllers/authenticate-controller.js
+++ login_authentication/controllers/authenticate-controller.js
@@ -1,6 +1,8 @@
 var bcrypt = require('bcrypt');
 var connection = require('./../config');
 const { json } = require('body-parser');
+var express = require("express");
+
 module.exports.authenticate = function(req,res){
     var email=req.body.email;
     var password=req.body.password;
@@ -15,10 +17,14 @@ module.exports.authenticate = function(req,res){
             if(results.length>0){
                bcrypt.compare(password,results[0].password,function(err,doesMatch){
                     if(doesMatch){
-                        const markup=`
-                        <center><h1>Welcome back ${results[0].name}</h1></center>;
-                       `;
-                       res.send(markup);
+                    //     const markup=`
+                    //     <center><h1>Welcome back ${results[0].name}</h1></center>;
+                    //    `;
+                    //    res.send(markup);
+                    const path="/mnt/d/workspace/loginform/Login_RegistrationForm/login_authentication";
+                  //  console.log(__dirname);
+                     res.sendFile(path+"/game1.html");
+
                     }else{
                         res.json({
                             status:false,
--- login_authentication/index.js
+++ login_authentication/index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var connection = require('./config');
 var app = express();
-
+var path=require('path');
 
 var authenticateController = require('./controllers/authenticate-controller');
 var registerController = require('./controllers/register-controller');
 
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
+app.use(express.static(path.join(__dirname+"/public")));
 app.get('/',function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
